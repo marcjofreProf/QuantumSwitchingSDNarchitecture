424dc2641dda87fc9ae17bb074b7525b3c2db133
marcjofreProf <119196997+marcjofreProf@users.noreply.github.com>
2026-08-30 20:07:53 +0200

    Update Helm chart versions for Atomix components

--- bootstrap-quantum-switching-sdn.sh
+++ bootstrap-quantum-switching-sdn.sh
@@ -577,6 +577,7 @@ deploy_cloud_native_uonos() {
     log_info "Purging stale Helm releases..."
     helm uninstall atomix-controller atomix-raft-storage onos-operator -n micro-onos 2>/dev/null || true
     helm uninstall atomix-controller atomix-raft-storage onos-operator -n kube-system 2>/dev/null || true
+    helm uninstall onos-topo onos-config -n micro-onos
 
     log_info "Removing legacy Atomix CRDs..."
     kubectl delete crd storageprofiles.atomix.io raftclusters.raft.atomix.io raftstores.raft.atomix.io --ignore-not-found 2>/dev/null || true
@@ -676,8 +677,8 @@ EOF
         --timeout=30s 2>/dev/null || true
 
     log_info "Installing v1beta3-compatible Atomix controllers and onos controllers..."
-    helm install atomix-controller atomix/atomix-controller -n kube-system
-    helm install atomix-raft-storage atomix/atomix-raft-storage -n kube-system --version 0.1.15
+    helm install atomix-controller atomix/atomix-controller -n kube-system --version 0.6.9
+    helm install atomix-raft-storage atomix/atomix-raft-storage -n kube-system --version 0.1.8
     helm install onos-topo onos/onos-topo -n micro-onos
     helm install onos-config onos/onos-config -n micro-onos
 
